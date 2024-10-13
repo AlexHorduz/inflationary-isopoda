a7199f1a6e444f8c20c8bfa2aceada2d86af35cd
TeeBeeaF <teebeeaf@gmail.com>
2024-10-13 20:57:28 +0300

    Merge remote-tracking branch 'origin/fba-branch' into fba-branch
    # Conflicts:
    #	WebInterface/authentication.py
    #	WebInterface/constants.py
    #	WebInterface/main.py
    #	WebInterface/sup.py

--- WebInterface/sup.py
+++ WebInterface/sup.py
@@ -1,7 +1,7 @@
 import jwt
+from datetime import datetime, timedelta, timezone
 
 from structure import UserInDB
-from datetime import datetime, timedelta, timezone
 from constants import JWT_SECRET, JWT_ALGORITHM, JWT_ACCESS_TOKEN_EXPIRE_MINUTES
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
